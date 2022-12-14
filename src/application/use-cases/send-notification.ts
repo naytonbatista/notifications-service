28e1870efe0a7fd4d15a74be62452513868c6afe
Nayton Batista <naytonbatista@gmail.com>
2022-12-14 00:01:47 -0400

    Reorganize files

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common';
 import { Content } from '../entities/content';
 import { Notification } from '../entities/notification';
 import { NotificationsRepository } from '../repositories/notifications-repository';
@@ -11,6 +12,8 @@ interface SendNotificationRequest {
 interface SendNotificationResponse {
   notification: Notification;
 }
+
+@Injectable()
 export class SendNotification {
   constructor(private repository: NotificationsRepository) {}
   async execute(
---
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../../../../application/entities/notification';
+import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaNotificationsRepository implements NotificationsRepository {
+  constructor(private service: PrismaService) {}
+  async create(notification: Notification): Promise<void> {
+    await this.service.notification.create({
+      data: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
