cf301d15165f3ec0445ed0707352096f784a48d4
Nayton Batista <naytonbatista@gmail.com>
2022-12-16 04:44:03 -0400

    Cancelar Notificação testes

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,37 @@
+import { Content } from '@application/entities/content';
+import { Notification } from '@application/entities/notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+describe('Cancel Notification', () => {
+  it(' should be able to cancel a notification', async () => {
+    const fakeRepository = new InMemoryNotificationsRepository();
+
+    const notification = new Notification({
+      category: 'test',
+      content: new Content('Nova Solicitação'),
+      recipientId: 'example-test-id',
+    });
+
+    await fakeRepository.create(notification);
+
+    const cancelNotification = new CancelNotification(fakeRepository);
+
+    await cancelNotification.execute({ notificationId: notification.id });
+
+    expect(fakeRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const fakeRepository = new InMemoryNotificationsRepository();
+
+    const cancelNotification = new CancelNotification(fakeRepository);
+
+    expect(() => {
+      return cancelNotification.execute({ notificationId: 'fake-id' });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
